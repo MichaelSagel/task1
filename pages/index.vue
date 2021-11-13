7b85ec9931b23a2abf25d9307de7cd97c86f1548
bojar <michael.sagel89@gmail.com>
2021-11-13 23:25:36 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,19 @@
 
         <headerArea/>
 
+      <contentArea/>
 
     </div>
 </template>
 
 <script>
   import headerArea from '../components/headerArea.vue';
+  import contentArea from '../components/contentArea.vue';
 
   export default {
     components: {
       headerArea,
+      contentArea
     }
   }
 </script>
