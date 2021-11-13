df16fc8484f785ee86f5a1aef3db49b741378c12
bojar <michael.sagel89@gmail.com>
2021-11-13 22:48:23 +0100

    test

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+      <div class="container">
+
+        <headerArea/>
+
+    </div>
+</template>
+
+<script>
+  import headerArea from '../components/headerArea.vue';
+
+  export default {
+    components: {
+      headerArea,
+    }
+  }
+</script>
+
+<style>
+  @import './../assets/styles/index.css';
+</style>
